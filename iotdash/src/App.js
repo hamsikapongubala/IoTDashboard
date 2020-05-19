3e05e5c28088476af8e030579a40e5bfb42239bf
Donie Ypon <donieypon99@gmail.com>
2020-05-18 21:51:59 -0700

    Emptied data folders

--- iotdash/src/App.js
+++ iotdash/src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import data from '../../myapp/data/doodle.png';
+import data from './data/doodle.png';
 
 function App() {
   return (
